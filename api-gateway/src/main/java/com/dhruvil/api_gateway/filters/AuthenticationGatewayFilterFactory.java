4577cb40245983dfde96d99cb2bc0ca358610b54
Dhruvil316 <dhruvil.developer@gmail.com>
2026-08-01 19:06:17 +0530

    014 : api-gateway-auth-filter-updated

--- api-gateway/src/main/java/com/dhruvil/api_gateway/filters/AuthenticationGatewayFilterFactory.java
+++ api-gateway/src/main/java/com/dhruvil/api_gateway/filters/AuthenticationGatewayFilterFactory.java
@@ -1,12 +1,16 @@
 package com.dhruvil.api_gateway.filters;
 
 import com.dhruvil.api_gateway.services.JwtService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Slf4j
@@ -23,22 +27,46 @@ public class AuthenticationGatewayFilterFactory extends AbstractGatewayFilterFac
     public GatewayFilter apply(Config config) {
         return (exchange,chain) -> {
             String authorisationHeader = exchange.getRequest().getHeaders().getFirst("Authorization") ;
+
             if (authorisationHeader == null || !authorisationHeader.startsWith("Bearer ")) {
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
             }
+
             String token = authorisationHeader.replace("Bearer ", "");
 
-//            Long userId = jwtService.getUserIdFromToken(token) ;
-
             try {
-                Long userId = jwtService.getUserIdFromToken(token);
-                exchange.getRequest()
-                        .mutate()
-                        .header("X-User-Id" , userId.toString() )
-                        .build() ;
+                Claims claims = jwtService.getClaims(token) ;
+                Long userId = claims.get("userId", Long.class);
+                String email = claims.getSubject();
+                List<String> roles = jwtService.getRoles(token);
+                List<String> permissions = jwtService.getPermissions(token);
 
-                return chain.filter(exchange);
+                ServerHttpRequest request =
+                        exchange.getRequest()
+                                .mutate()
+
+                                // Remove any client supplied values
+                                .headers(headers -> {
+                                    headers.remove("X-User-Id");
+                                    headers.remove("X-Email");
+                                    headers.remove("X-Roles");
+                                    headers.remove("X-Permissions");
+                                })
+
+                                // Add trusted values
+                                .header("X-User-Id", userId.toString())
+                                .header("X-Email", email)
+                                .header("X-Roles", String.join(",", roles))
+                                .header("X-Permissions", String.join(",", permissions))
+
+                                .build();
+
+                return chain.filter(
+                        exchange.mutate()
+                                .request(request)
+                                .build());
+
             } catch (JwtException e) {
 
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
--- api-gateway/src/main/java/com/dhruvil/api_gateway/services/JwtService.java
+++ api-gateway/src/main/java/com/dhruvil/api_gateway/services/JwtService.java
@@ -3,14 +3,16 @@ package com.dhruvil.api_gateway.services;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.io.Decoders;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Service
+@Slf4j
 public class JwtService {
 
     @Value("${jwt.secretKey}")
@@ -23,16 +25,31 @@ public class JwtService {
         );
     }
 
-    public Long getUserIdFromToken(String token) {
+    public Claims getClaims (String token) {
 
-        Claims claims = Jwts.parser()
+        return Jwts.parser()
                 .verifyWith(getSecretKey())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
 
-        return claims.get("userId", Long.class);
     }
 
+    public List<String> getRoles(String token) {
+        Claims claims = getClaims(token);
 
+        return ((List<?>) claims.get("roles"))
+                .stream()
+                .map(Object::toString)
+                .toList();
+    }
+
+    public List<String> getPermissions(String token) {
+        Claims claims = getClaims(token);
+
+        return ((List<?>) claims.get("permissions"))
+                .stream()
+                .map(Object::toString)
+                .toList();
+    }
 }
